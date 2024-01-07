d6e9f97678b7823eb64bc8d0a04228a81a7efda1
Ali Aizad <aliaizad72@gmail.com>
2024-01-07 20:14:02 +0800

    Complete refactoring char class

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -3,12 +3,41 @@
 # A program to cipher text the roman emperor way
 class CaesarCipher
   def cipher(str, shift_factor = 1)
-    str_array = str.split('')
-    str_ord_array = str_array.map(&:ord)
+    str_array = str.split('').map { |char| CaesarChar.new(char, char.ord) }
+    str_array.each_with_index do |char, ind|
+      # Ignoring shift for non-alphabet
+      if char.alphabet?
+        shift_ord = char.ord + shift_factor
+        # Correct the shift if it is the shift went past the upper limit of upcase and lowcase
+        shift_ord -= 26 if shift_ord > 'Z'.ord && char.upcase? || shift_ord > 'z'.ord && char.downcase?
+        str_array[ind] = shift_ord
+      else
+        str_array[ind] = char.ord
+      end
+    end
+    shift_str = str_array.map(&:chr)
+    shift_str.join('')
   end
 end
 
-upper_case = ('A'..'Z').to_a
-lower_case = ('a'..'z').to_a
-alphabet_arr = upper_case + lower_case
-p alphabet_arr.map(&:ord)
+class CaesarChar
+  attr_reader :char, :ord
+
+  def initialize(char, ord)
+    @char = char
+    @ord = ord
+  end
+
+  def alphabet?
+    upcase? || downcase?
+  end
+
+  def upcase?
+    ord.between?('A'.ord, 'Z'.ord)
+  end
+
+  def downcase?
+    ord.between?('a'.ord, 'z'.ord)
+  end
+end
+  
--- spec/caesar_cipher.rb
+++ spec/caesar_cipher.rb
@@ -5,28 +5,24 @@ require './lib/caesar_cipher.rb'
 describe CaesarCipher do
   cipher_machine = CaesarCipher.new
   describe '#cipher' do
-    it 'ciphers one word with the shift factor of 1' do
-      expect(cipher_machine.cipher('peanut', 1)).to eql('qfbovu')
+    it 'ciphers one word with the shift factor of 19' do
+      expect(cipher_machine.cipher('peanut', 19)).to eql('ixtgnm')
     end
 
-    it 'ciphers one word with punctuation with shift factor of 1' do
-      expect(cipher_machine.cipher('peanut!', 1)).to eql('qfbovu!')
+    it 'ciphers one word with punctuation with shift factor of 19' do
+      expect(cipher_machine.cipher('peanut!', 19)).to eql('ixtgnm!')
     end
 
-    it 'ciphers one word with capitalisation with shift factor of 1' do
-      expect(cipher_machine.cipher('Peanut', 1)).to eql('Qfbovu')
+    it 'ciphers one word with capitalisation with shift factor of 19' do
+      expect(cipher_machine.cipher('Peanut', 19)).to eql('Ixtgnm')
     end
 
-    it 'ciphers one word with capitalisation and punctuation with shift factor of 1' do
-      expect(cipher_machine.cipher('Peanut!', 1)).to eql('Qfbovu!')
-    end
-
-    it 'ciphers one word with shift factor of 11' do
-      expect(cipher_machine.cipher('peanut', 11)).to eql('aplyfe')
+    it 'ciphers one word with capitalisation and punctuation with shift factor of 19' do
+      expect(cipher_machine.cipher('Peanut!', 19)).to eql('Ixtgnm!')
     end
 
     it 'ciphers multiple word sentence with punctuation and capitalisation, with shift factor of 13' do
-      expect(cipher_machine.cipher('Ave Imperator! Morituri te salutant.', 14)).to eql('Nir Vzcrengbe! Zbevghev gr fnyhgnag.')
+      expect(cipher_machine.cipher('Ave Imperator! Morituri te salutant.', 13)).to eql('Nir Vzcrengbe! Zbevghev gr fnyhgnag.')
     end
   end
 end
